92ba1566c8e17a8a844984f6ac40590b22f74847
SHAHAB UDDIN <shahab.82946@gmail.com>
2024-11-25 07:01:14 +0600

    mongodb client practice

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+
+function App() {
+
+  const handleAddUser = (e)=> {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email}
+    console.log(user)
+
+    fetch('http://localhost:5000/users', {
+      method: "POST",
+      headers: {
+        "content-type" : "application/json"
+
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    
+  }
+  return (
+    <>
+      <h1>SIMPLE CRUD</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" placeholder="name" />
+        <br />
+
+        <input type="email" name="email" id=""  placeholder="email"/> <br />
+        <input type="submit" value="add user" />
+
+      </form>
+    </>
+  );
+}
+
+export default App;
